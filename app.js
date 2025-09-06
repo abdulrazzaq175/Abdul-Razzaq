1be38586e176f7393b2479294ecc103bdbc74541
abdulrazzaq175 <abdul464732@gmail.com>
2025-09-06 05:13:32 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-let role=prompt("What is your position in this company(Admin,Manager,Employ or an Intern)")
-let access=false;
- 
-if(role=="admin"||role=="Admin")
+let name=prompt("Enter your name")
+document.write("<b>Welcom ", name,"<br><br><b>");
+let t=prompt("Enter any number for which table is required")
+document.write("The table of "+t +" is written below:<br><br>") ;
+
+for(let i=0;i<21;i++)
 {
-   access=true;  
+     document.write(t + " x " + i + " = " + (t * i),"<br>");   
 }
-else if(role=="Manager"||role=="manager")
-{
-  let experience=prompt("Enter number of years of your experience")
-  if(experience>=2)
-      access=true;  
-}
-else if(role=="Employ"||role=="employ")
-{
-   let experience=prompt("Enter number of years of your experience")
-  let dept=prompt("What is your department ?")
-  if(experience>3 && ((dept=="It" || dept=="it") || (dept=="Hr" || dept=="hr"))){
-    access=true;}
-}
-else if(role=="intern"||role=="Intern")
-{
-  access=false;
-}
-  if (access) {
-      console.log("Access Granted");
-    } else {
-      console.log("Access Denied");
-    }
